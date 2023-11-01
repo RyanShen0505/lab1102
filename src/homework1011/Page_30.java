80e10c4e1e4cba94fe14df796b3f6e76f00fd9cb
RyanShen0505 <shenryan0505@gmail.com>
2023-11-02 00:22:43 +0800

    j

---
+++ src/homework1011/Page_30.java
@@ -0,0 +1,12 @@
+package homework1011;
+
+public class Page_30 {
+	public static void main(String[] args) {
+		int i,j;
+		for(i=0;i<5;i++) {
+			for(j=0;j<3;j++) {
+				System.out.println("i圈"+i+":j圈"+j);
+			}
+		}
+	}
+}
